d3f60dd3d079c8871a64d68953cc43b95952c984
butschster <butschster@gmail.com>
2023-10-01 12:52:36 +0400

    Improved type information with psalm annotations on entity factory

--- src/Factory/FactoryInterface.php
+++ src/Factory/FactoryInterface.php
@@ -4,23 +4,56 @@ declare(strict_types=1);
 
 namespace Spiral\DatabaseSeeder\Factory;
 
+/**
+ * @template TEntity of object
+ */
 interface FactoryInterface
 {
-    /** @psalm-return class-string */
+    /**
+     * Get the entity class name.
+     *
+     * @psalm-return class-string<TEntity>
+     */
     public function entity(): string;
-
-    public function definition(): array;
 
     public static function new(): static;
 
-    /** @psalm-param positive-int $amount */
+    /**
+     * Entity data definition. This data will be used to create an entity.
+     */
+    public function definition(): array;
+
+    /**
+     * How many entities should be created.
+     *
+     * @psalm-param positive-int $amount
+     */
     public function times(int $amount): self;
 
+    /**
+     * Create many entities with persisting them to the database.
+     *
+     * @return TEntity[]
+     */
     public function create(): array;
 
+    /**
+     * Create an entity with persisting it to the database.
+     *
+     * @return TEntity
+     */
     public function createOne(): object;
 
+    /**
+     * Make many entities without persisting them to the database.
+     *
+     * @return TEntity[]
+     */
     public function make(): array;
 
+    /**
+     * Make an entity without persisting it to the database.
+     * @return TEntity
+     */
     public function makeOne(): object;
 }
