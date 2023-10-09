12c9d03b3f25ca41e697178d18d248db0ad02230
Maxim Smakouz <m.smakouz@gmail.com>
2023-10-09 13:08:15 +0300

    Add SQLFileStrategy tests

---
+++ tests/src/Functional/Database/Strategy/SQLFileStrategyTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Functional\Database\Strategy;
+
+use Cycle\Database\DatabaseProviderInterface;
+use Spiral\DatabaseSeeder\Database\Strategy\SqlFileStrategy;
+use Spiral\DatabaseSeeder\Database\Traits\DatabaseAsserts;
+use Tests\Functional\TestCase;
+
+final class SQLFileStrategyTest extends TestCase
+{
+    use DatabaseAsserts;
+
+    public function testExecute(): void
+    {
+        $this->assertTable('users')->assertMissing();
+
+        $strategy = new SqlFileStrategy(
+            \dirname(__DIR__, 4) . '/app/database/sql/execute.sql',
+            $this->getContainer()->get(DatabaseProviderInterface::class)
+        );
+        $strategy->execute();
+
+        $this->assertTable('users')->assertExists();
+        $this->assertTable('users')->assertCountRecords(5);
+
+        $this->cleaner->dropTable('users');
+    }
+
+    public function testExecuteAndDrop(): void
+    {
+        $this->assertTable('users')->assertMissing();
+
+        $strategy = new SqlFileStrategy(
+            \dirname(__DIR__, 4) . '/app/database/sql/execute.sql',
+            $this->getContainer()->get(DatabaseProviderInterface::class),
+            \dirname(__DIR__, 4) . '/app/database/sql/drop.sql',
+        );
+        $strategy->execute();
+
+        $this->assertTable('users')->assertExists();
+        $this->assertTable('users')->assertCountRecords(5);
+
+        $strategy->drop();
+
+        $this->assertTable('users')->assertMissing();
+    }
+}
